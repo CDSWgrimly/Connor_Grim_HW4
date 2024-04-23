9d0359d855b5c4bb2b0e23b0a03933082405186e
CDSWgrimly <grim.connor@gmail.com>
2024-04-23 10:40:07 -0500

    Initial Commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Main {
+    public static String encodingString;
+    public static void main(String[] args) {
+        MsgTree tree = new MsgTree(encodingString);
+    }
+}
